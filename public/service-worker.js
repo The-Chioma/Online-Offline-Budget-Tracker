685d2b4227c0f7fd51d5738193c15eba51f9605f
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2021-03-22 16:47:22 +0000

    cache functionality in service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,55 @@
 // creating cache variables
-var CACHE = "budget-cache";
-var DATA_CACHE = "budget-data-cache";
+const CACHE = "budget-cache";
+const DATA_CACHE = "budget-data-cache";
 
 // important pages that need to be saved in the cache
-var appUrlArray = [
-    "/",
-    "/db.js",
-    "/index.js",
-    "/styles.css",
-    "/manifest.json",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-]
+const appUrlArray = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/styles.css",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
 
-window.addEventListener("install", function(e) {
-    e.waitUntil(
-        caches.open(CACHE).then(function(cache) {
-            return cache.addAll(appUrlArray)
+//adding event listener to install cache
+window.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      cache.addAll(appUrlArray);
+    })
+  );
+});
+
+//fetches the request and displays cached data
+window.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("/api/")) {
+    e.respondWith(
+      caches
+        .open(DATA_CACHE)
+        .then((cache) => {
+          return fetch(e.request)
+            .then((res) => {
+              if (res.status === 200) {
+                cache.put(e.request.url, res.clone());
+              }
+              return res;
+            })
+            .catch(() => {
+              return cache.match(e.request);
+            });
         })
-    )
-}) 
+        .catch((err) => console.log(err))
+    ).catch;
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request).then((res) => {
+        return res;
+      });
+    })
+  );
+});
